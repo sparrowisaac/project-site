401e52bdcaba2366c16bc8f3473eeaff473873a9
Isaac Sparrow <sparrowi@unc.edu>
2023-05-02 23:36:03 -0400

    New Edits

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app: Flask = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route('/create-page1')
+def create_todo():
+    return render_template("create-page1.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
