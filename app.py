6adf8edc767d4bdaa0b849ef4bbfaeb2861eca72
Isaac Sparrow <sparrowi@unc.edu>
2023-05-02 23:49:01 -0400

    Trying to get it working

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template
+# from flask import Flask, render_template
 
-app: Flask = Flask(__name__)
+# app: Flask = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template('index.html')
+# @app.route("/")
+# def index():
+#     return render_template('index.html')
 
-@app.route('/create-page1')
-def create_todo():
-    return render_template("create-page1.html")
+# @app.route('/create-page1')
+# def create_todo():
+#     return render_template("create-page1.html")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
